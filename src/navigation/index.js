66cb5e45d34bcfc4d054d6fb8121e6a206604c52
sonjunho2 <sonjunho2@gmail.com>
2025-08-21 16:04:59 +0900

    Update index.js

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../context/AuthContext';
 import colors from '../theme/colors';
 
 // Auth Screens

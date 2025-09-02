10c0fec4708dadbddcfdc74e51320ac88b00995b
sonjunho2 <sonjunho2@gmail.com>
2025-09-02 16:46:33 +0900

    Update RootNavigator.js

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -4,7 +4,7 @@ import { View, ActivityIndicator } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../context/AuthContext';
 
 import LoginScreen from '../screens/auth/LoginScreen';
 import SignupScreen from '../screens/auth/SignupScreen';

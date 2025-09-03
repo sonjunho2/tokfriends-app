c3a04d4187e704f262d253434e923f579610a6ce
sonjunho2 <sonjunho2@gmail.com>
2025-09-03 11:24:08 +0900

    Create typography.js

---
+++ src/theme/typography.js
@@ -0,0 +1,5 @@
+// src/theme/typography.js
+export const H1 = { fontSize: 28, fontWeight: '800', letterSpacing: -0.5 };
+export const H2 = { fontSize: 22, fontWeight: '800' };
+export const Body = { fontSize: 16 };
+export const Caption = { fontSize: 12 };

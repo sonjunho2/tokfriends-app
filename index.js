9e1409653d047fdd59c7fa94620e6884eb865583
sonjunho2 <sonjunho2@gmail.com>
2025-09-03 09:05:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // index.js
+import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 import App from './App';
 
 registerRootComponent(App);
+export default App; // ★ Snack이 필요로 하는 default export

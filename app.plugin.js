91acb31b79419b9f15f2a867d0a7037812d952ed
sonjunho2 <sonjunho2@gmail.com>
2025-09-07 12:21:19 +0900

    Update app.plugin.js

--- app.plugin.js
+++ app.plugin.js
@@ -2,15 +2,24 @@
 /** @type {import('@expo/config-plugins').ConfigPlugin} */
 const { withAppBuildGradle } = require('@expo/config-plugins');
 
+// 어떤 형태든 enableBundleCompression 토큰을 제거
 const stripEnableBundleCompression = (src) => {
-  // 1) 라인 단위로 있는 모든 enableBundleCompression 제거
-  let out = src.replace(/^[\t ]*enableBundleCompression\s*=?\s*(true|false)\s*[\r]?\n/gm, '');
+  // 1) 단독 라인/할당/무할당(true/false) 모두 제거
+  let out = src.replace(
+    /^[\t ]*enableBundleCompression(?:\s*=?\s*(?:true|false))?\s*[\r]?\n/gm,
+    ''
+  );
 
-  // 2) react { ... } 블록 내부에 섞여있는 경우도 정리
+  // 2) react { ... } 블록 내부에 섞여있는 경우도 제거
   out = out.replace(/react\s*\{([\s\S]*?)\}/gm, (m) => {
-    const cleaned = m.replace(/^[\t ]*enableBundleCompression\s*=?\s*(true|false)\s*[\r]?\n/gm, '');
-    return cleaned;
+    return m.replace(
+      /^[\t ]*enableBundleCompression(?:\s*=?\s*(?:true|false))?\s*[\r]?\n/gm,
+      ''
+    );
   });
+
+  // 3) 남아있는 토큰(주석 옆 등)까지 광역 치환
+  out = out.replace(/enableBundleCompression(?:\s*=?\s*(?:true|false))?/g, '');
 
   return out;
 };

d618c59baf5a01351203c0d9efbd3f2b4aa9fa87
sonjunho2 <sonjunho2@gmail.com>
2025-09-08 16:02:45 +0900

    Update ShopScreen.js

--- src/screens/shop/ShopScreen.js
+++ src/screens/shop/ShopScreen.js
@@ -2,16 +2,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import colors from '../../theme/colors';
-
 export default function ShopScreen() {
   return (
-    <View style={s.c}>
-      <Text style={s.t}>상점 준비중</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>상점 (준비중)</Text>
     </View>
   );
 }
-
-const s = StyleSheet.create({
-  c:{ flex:1, alignItems:'center', justifyContent:'center', backgroundColor:colors.background },
-  t:{ fontSize:18, color:colors.text, fontWeight:'700' },
+const styles = StyleSheet.create({
+  container: { flex:1, alignItems:'center', justifyContent:'center', backgroundColor: colors.background },
+  title: { fontSize: 20, fontWeight: '800', color: colors.text },
 });

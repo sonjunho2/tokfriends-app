115924ecac0194ba50081861de58cd7cb3bcef82
sonjunho2 <sonjunho2@gmail.com>
2025-09-08 13:17:14 +0900

    Update app.plugin.js

--- app.plugin.js
+++ app.plugin.js
@@ -1,21 +1,3 @@
 // app.plugin.js
-/** @type {import('@expo/config-plugins').ConfigPlugin} */
-const { withAppBuildGradle } = require('@expo/config-plugins');
-
-/**
- * react { } 블록 내부 및 외부에서 enableBundleCompression 라인을 제거한다.
- */
-function stripEnableBundleCompression(src) {
-  return src
-    .split(/\r?\n/)
-    .filter((line) => !/\benableBundleCompression\b/.test(line))
-    .join('\n');
-}
-
-module.exports = (config) =>
-  withAppBuildGradle(config, (cfg) => {
-    if (cfg.modResults && typeof cfg.modResults.contents === 'string') {
-      cfg.modResults.contents = stripEnableBundleCompression(cfg.modResults.contents);
-    }
-    return cfg;
-  });
+// 외부 의존성 없이 동작하는 no-op 플러그인 (빌드 파이프라인 안정화용)
+module.exports = (config) => config;

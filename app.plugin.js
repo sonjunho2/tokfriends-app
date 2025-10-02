71c270ea5c3768f63d57ab17bc8bea90ca67f85a
sonjunho2 <sonjunho2@gmail.com>
2025-10-02 10:04:08 +0900

    Update app.plugin.js

--- app.plugin.js
+++ app.plugin.js
@@ -1,17 +1,19 @@
-// 새파일 또는 교체
-// 목적: RN 0.76 Gradle 플러그인에서 제거된 `enableBundleCompression` 속성 라인을 app/build.gradle에서 제거
+// 교체: app/build.gradle(혹은 .kts)에서 enableBundleCompression 라인 전부 제거
 const { withAppBuildGradle } = require('@expo/config-plugins');
 
 module.exports = function withFixEnableBundleCompression(config) {
   return withAppBuildGradle(config, (cfg) => {
     let src = cfg.modResults.contents;
 
-    // 1) enableBundleCompression 라인 제거 (Groovy/KTS 모두 대응)
-    const lineRegex = /^[ \t]*enableBundleCompression\s*=?\s*(true|false).*$/gmi;
-    src = src.replace(lineRegex, '');
+    // Groovy/KTS 어떤 형태라도 'enableBundleCompression'를 포함한 라인을 전부 삭제
+    // 예) enableBundleCompression = false
+    // 예) enableBundleCompression false
+    // 예) enableBundleCompression.set(true)
+    // 예) react { enableBundleCompression = false }
+    const anyLineWithProp = /^.*enableBundleCompression.*$/gmi;
+    src = src.replace(anyLineWithProp, '');
 
-    // 2) react { } 블록 내에 남은 공백 정리(선택적)
-    // react { ... } 블록이 비어 있어도 빌드에는 영향 없음. 안전 차원에서 공백만 정리.
+    // react 블록 내 공백 최소화
     src = src.replace(/\n{3,}/g, '\n\n');
 
     cfg.modResults.contents = src;

beb9e60b4944a646d6d6020b93169f9def2980dd
sonjunho2 <sonjunho2@gmail.com>
2025-10-02 12:50:41 +0900

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,40 +1,32 @@
-export default ({ config }) => {
-  const isCI = process.env.EAS_BUILD === 'true' || process.env.CI === 'true';
-
-  return {
-    ...config,
-    android: {
-      ...(config.android ?? {}),
-      package: 'com.sonjunho.ddakchin',
+export default ({ config }) => ({
+  ...config,
+  android: {
+    ...(config.android ?? {}),
+    package: 'com.sonjunho.ddakchin',
+  },
+  plugins: [
+    ...(config.plugins ?? []),
+    [
+      'expo-build-properties',
+      {
+        android: {
+          kotlinVersion: '2.0.21',
+          gradlePluginVersion: '8.7.2',
+          compileSdkVersion: 35,
+          targetSdkVersion: 35,
+          minSdkVersion: 24
+        }
+      }
+    ]
+  ],
+  extra: {
+    ...(config.extra ?? {}),
+    apiBaseUrl:
+      process.env.EXPO_PUBLIC_API_BASE_URL ??
+      config?.extra?.apiBaseUrl ??
+      'https://tok-friends-api.onrender.com',
+    eas: {
+      projectId: 'eb3c1b74-5c41-4ce0-9574-d0d3eb932d72',
     },
-    plugins: [
-      ...(config.plugins ?? []),
-      ...(isCI
-        ? [
-            [
-              'expo-build-properties',
-              {
-                android: {
-                  kotlinVersion: '2.0.21',
-                  gradlePluginVersion: '8.7.2',
-                  compileSdkVersion: 35,
-                  targetSdkVersion: 35,
-                  minSdkVersion: 24,
-                },
-              },
-            ],
-          ]
-        : []),
-    ],
-    extra: {
-      ...(config.extra ?? {}),
-      apiBaseUrl:
-        process.env.EXPO_PUBLIC_API_BASE_URL ??
-        config?.extra?.apiBaseUrl ??
-        'https://tok-friends-api.onrender.com',
-      eas: {
-        projectId: 'eb3c1b74-5c41-4ce0-9574-d0d3eb932d72',
-      },
-    },
-  };
-};
+  },
+});

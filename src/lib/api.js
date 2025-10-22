df4712cb55f1f97f9fdb05453bd95e7651cbd4b0
sonjunho2 <sonjunho2@gmail.com>
2025-10-22 16:05:56 +0900

    Delete src/lib/api.js

--- src/lib/api.js
+++
@@ -1,6 +0,0 @@
-// Deprecated: Use src/api/client.js instead
-// This file is kept for backward compatibility
-import client from '../api/client';
-
-export const api = client;
-export default client;

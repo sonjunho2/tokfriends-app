9ab5cefc1cff6b225b7b2169cbb2af4e67d745ac
sonjunho2 <sonjunho2@gmail.com>
2025-10-31 15:41:01 +0900

    Delete src/api/community.js

--- src/api/community.js
+++
@@ -1,9 +0,0 @@
-import { apiClient } from './client';
-
-export async function reportUser({ reporterId, targetUserId, reason, postId = null }) {
-  return await apiClient.reportUser({ reporterId, targetUserId, reason, postId });
-}
-
-export async function blockUser({ blockerId, targetUserId }) {
-  return await apiClient.blockUser({ blockerId, targetUserId });
-}

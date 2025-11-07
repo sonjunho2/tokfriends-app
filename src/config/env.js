e1eeaa049227607e24e0fd1a1b860451049849cd
sonjunho2 <sonjunho2@gmail.com>
2025-11-07 15:49:39 +0900

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,9 @@
+import appConfig from '../../app.config';
 import Constants from 'expo-constants';
 
 // 환경변수 우선순위: EXPO_PUBLIC_ > app.json extra > 기본값
-const fromPublic = process.env.EXPO_PUBLIC_API_BASE_URL;
-const fromExtra = Constants?.expoConfig?.extra?.apiBaseUrl || Constants?.manifest?.extra?.apiBaseUrl;
+// const fromPublic = process.env.EXPO_PUBLIC_API_BASE_URL;
+// const fromExtra = Constants?.expoConfig?.extra?.apiBaseUrl || Constants?.manifest?.extra?.apiBaseUrl;
 
 export const API_BASE_URL = (process.env.TOK_API_BASE_URL || appConfig?.extra?.TOK_API_BASE_URL || 'https://tok-friends-api.onrender.com').replace(/\/$/, '');
 export const REQUEST_TIMEOUT_MS = 10000;
